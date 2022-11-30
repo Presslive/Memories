23fe0737ce6eaa9983ad3bf19287047e7e0d5bb3
Diallo Abdoulaye <lingodir@gmail.com>
2022-11-30 04:09:24 -0500

    Server Side

---
+++ src/components/Post/Posts.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Post from './Post/Post';
+
+const Posts = () => {
+    return (
+        <>
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+
+        </>
+    );
+};
+
+export default Posts;
